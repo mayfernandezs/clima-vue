2a3c83e0b7c04614f68cf63316a53282545f32b3
MayFernandezS <183709108+MayFernandezS@users.noreply.github.com>
2025-01-13 16:31:14 +0100

    fix

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -17,7 +17,7 @@ export default function useClima() {
     
     try {
       // Obtener la latitud y longitud
-      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+      const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
       const { data } = await axios(url)
       const { lat, lon } = data[0]
       
